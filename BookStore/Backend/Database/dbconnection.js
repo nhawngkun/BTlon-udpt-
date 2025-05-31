7c7fc23611757455ab79025e30ae36e596f68d37
LanAnhDao204 <22010262@st.phenikaa-uni.edu.vn>
2025-05-31 15:41:16 +0700

    fix backend

--- BookStore/Backend/Database/dbconnection.js
+++ BookStore/Backend/Database/dbconnection.js
@@ -13,4 +13,7 @@ const driver = neo4j.driver(
     neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
 );
 
+// Nếu có đoạn hardcode password thì sửa lại như sau (nếu không có thì bỏ qua):
+// neo4j.auth.basic('neo4j', 'UlXPYheImRAqEPXhXehOLc89qRc9AKM6us2x2VJHgkY')
+
 export default driver;
