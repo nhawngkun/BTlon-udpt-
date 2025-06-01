4b621a7de6a11fa2b796458cef82f1f2c7d1cc45
LanAnhDao204 <22010262@st.phenikaa-uni.edu.vn>
2025-06-01 23:53:44 +0700

    sửa config

--- BookStore/Frontend/src/config.js
+++ BookStore/Frontend/src/config.js
@@ -1,8 +1,4 @@
 // Config for backend API URL
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  process.env.REACT_APP_API_URL ||
-  "https://bookstore-backend-production.up.railway.app" || // URL backend đã deployed
-  "http://localhost:8881";
-
+const API_URL = "https://bs-noqz.onrender.com";
+console.log("Backend URL loaded:", API_URL);
 export default API_URL;
