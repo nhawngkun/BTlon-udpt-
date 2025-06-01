4dfbc49983a78b2490c030b705e13534f915816b
LanAnhDao204 <22010262@st.phenikaa-uni.edu.vn>
2025-06-01 23:58:06 +0700

    h

--- BookStore/Frontend/src/Components/Navbar.jsx
+++ BookStore/Frontend/src/Components/Navbar.jsx
@@ -72,16 +72,20 @@ const Navbar = () => {
 
     const navItems = (
         <>
-            <li className="rounded-md dark:hover:bg-pink-500 duration-200"><a href="/">Trang chủ</a></li>
-            <li className="rounded-md dark:hover:bg-pink-500 duration-200"><a href="/profile">Hồ sơ</a></li>
+            <li className="rounded-md dark:hover:bg-pink-500 duration-200">
+                <Link to="/">Trang chủ</Link>
+            </li>
+            <li className="rounded-md dark:hover:bg-pink-500 duration-200">
+                <Link to="/profile">Hồ sơ</Link>
+            </li>
             {isAdmin && (
                 <li className="rounded-md dark:hover:bg-pink-500 duration-200">
-                    <a href="/admin" className="flex items-center">
+                    <Link to="/admin" className="flex items-center">
                         <span className="mr-1">Admin</span>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
                         </svg>
-                    </a>
+                    </Link>
                 </li>
             )}
         </>
